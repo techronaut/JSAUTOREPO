1198819f881b39a187c29d39a86b1d8d88f61e5e
Joseph A. Scaravilli <joe.scaravilli@peters.com>
2017-03-09 09:35:28 -0600

    Add files via upload

---
+++ DSCNodeConfigurations/BaseServerConfig.ps1
@@ -0,0 +1,21 @@
+﻿#Requires -version 5.0
+Configuration BaseServerConfig
+    {
+    param (
+          )
+    $zone = "Central Standard Time"
+    $DrvLtr = "Z:"
+    Import-DscResource –ModuleName PSDesiredStateConfiguration,
+                                   xTimezone,
+                                   cCDROMdriveletter
+
+        xTimeZone zone {
+            IsSingleInstance = 'Yes'
+            TimeZone = $zone  
+         }
+
+         cCDROMdriveletter localhost {
+            DriveLetter = $DrvLtr
+            Ensure = 'Present'
+         }
+}
